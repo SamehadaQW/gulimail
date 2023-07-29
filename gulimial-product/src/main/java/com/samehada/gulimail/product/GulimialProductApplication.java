f7b8519eacdc863a5e93266b78e765a9d58df6e7
samehada <971650778@qq.com>
2023-07-29 22:25:16 +0800

    初始项目目录结构创建

---
+++ gulimial-product/src/main/java/com/samehada/gulimail/product/GulimialProductApplication.java
@@ -0,0 +1,13 @@
+package com.samehada.gulimail.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimialProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimialProductApplication.class, args);
+    }
+
+}
---
+++ gulimial-product/src/test/java/com/samehada/gulimail/product/GulimialProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samehada.gulimail.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimialProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
